e77a93a0f8bd348ff6ff25604884a9bef2dee7bf
nasubi916 <r916nis@gmail.com>
2025-03-03 14:37:58 +0900

    Shopフェーズで最初からカード選択に移行

--- src/components/shop.vue
+++ src/components/shop.vue
@@ -3,14 +3,10 @@ import { ref, watch, onMounted } from "vue";
 import { playerStore } from "@/main";
 import { storeToRefs } from "pinia";
 import ShopOffer from "./shopOffer.vue";
-import drawCardImg from "@/assets/img/ui/drawCard.png";
-import useGiftImg from "@/assets/img/ui/useGift.png";
 import shoppingGif from "@/assets/gifs/shopping.gif";
-import back from "@/assets/img/ui/back.png";
 
 import { useSound } from "@vueuse/sound";
 import { tap2, shopping } from "@/assets/sounds";
-const useTap2 = useSound(tap2);
 const useShopping = useSound(shopping);
 
 const { cardLock } = storeToRefs(playerStore);
@@ -32,11 +28,6 @@ const loadShoppingGif = () => {
     shopAnimation.value = false;
   }, 1000);
 };
-const returnShop = () => {
-  use.value = false;
-  draw.value = false;
-  useTap2.play();
-};
 </script>
 
 <template>
@@ -53,18 +44,7 @@ const returnShop = () => {
         <img @load="loadShoppingGif()" :src="shoppingGif" />
       </div>
       <div v-else class="overlay">
-        <ShopOffer v-show="draw" />
-        <div v-if="!draw && !use">
-          <button
-            @click="
-              draw = true;
-              useTap2.play();
-            "
-            style="width: 20vw"
-          >
-            <img :src="drawCardImg" />
-          </button>
-        </div>
+        <ShopOffer />
       </div>
     </transition>
   </div>
