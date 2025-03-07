b365db53272af248774338d4ef291619d7c56b2e
nasubi916 <r916nis@gmail.com>
2025-03-08 05:43:38 +0900

    特殊効果表示

--- src/components/uiCard.vue
+++ src/components/uiCard.vue
@@ -43,11 +43,8 @@ watch(
 
 <template>
   <div class="">
-    <div
-      v-if="dropDown"
-      class="z-20 mx-3 fixed text-gray-900 text-left"
-      :class="[card.description ? `w-[max(20vw,340px)] -translate-y-32` : `w-[max(15vw,180px)] -translate-y-24`, state ? `` : `max-w-fit`]"
-    >
+    <div v-if="dropDown" class="z-20 mx-3 fixed text-gray-900 text-left"
+      :class="[card.description ? `w-[max(20vw,340px)] -translate-y-32` : `w-[max(15vw,180px)] -translate-y-24`, state ? `` : `max-w-fit`]">
       <div :class="[card.description ? `w-[max(20vw,340px)]` : `w-[max(15vw,180px)]`]">
         <img :src="bg" class="z-20 absolute" />
         <div class="z-20 p-4 px-5 absolute">
@@ -60,32 +57,22 @@ watch(
       <VDuringPress :onKeyDown="onLongPressCallbackHook" :onKeyUp="onKeyUpCallbackHook" :delay="250">
         <img :src="`/img/companys/${card.company}.png`" />
         <div class="overText">
-          <div
-            v-if="card.waste"
-            class="font-bold text-center select-none"
-            :class="[
-              size === 'normal'
-                ? `text-[min(120%,1.5rem)] -translate-x-[min(7vw,1.9rem)] translate-y-[min(4.2vw,1.3rem)]`
-                : `text-[min(170%,1.8rem)] -translate-x-[min(11vw,2.7rem)] translate-y-[min(6vw,1.7rem)]`,
-              card.waste === 1 ? `-translate-x-[380%]` : null,
-            ]"
-          >
+          <div v-if="card.waste" class="font-bold text-center select-none" :class="[
+            size === 'normal'
+              ? `text-[min(120%,1.5rem)] -translate-x-[min(7vw,1.9rem)] translate-y-[min(4.2vw,1.3rem)]`
+              : `text-[min(170%,1.8rem)] -translate-x-[min(11vw,2.7rem)] translate-y-[min(6vw,1.7rem)]`,
+            card.waste === 1 ? `-translate-x-[380%]` : null,
+          ]">
             <div :class="wastedClass" class="absolute">
               {{ card.waste }}
             </div>
           </div>
           <div class="p-2" />
-          <img
-            v-if="card.waste"
-            :src="`/img/foods/${card.id}.png`"
-            class="transform"
-            :class="size === 'normal' ? `-translate-x-1 translate-y-2` : `-translate-x-[10%] translate-y-[20%]`"
-          />
+          <img v-if="card.waste" :src="`/img/foods/${card.id}.png`" class="transform"
+            :class="size === 'normal' ? `-translate-x-1 translate-y-2` : `-translate-x-[10%] translate-y-[20%]`" />
 
-          <div
-            class="flex font-black text-border-thin transform select-none"
-            :class="size === 'normal' ? `text-[min(70%,1rem)] -translate-x-[10%]` : `text-[min(130%,1.5rem)] -translate-x-[10%]`"
-          >
+          <div class="flex font-black text-border-thin transform select-none"
+            :class="size === 'normal' ? `text-[min(70%,1rem)] -translate-x-[10%]` : `text-[min(130%,1.5rem)] -translate-x-[10%]`">
             <p v-if="card.hungry !== undefined && card.id !== 0">{{ "🍖" + card.hungry }}</p>
             <p v-if="card.atk">{{ "💪" + card.atk }}</p>
             <p v-if="card.def">{{ "🛡" + card.def }}</p>
@@ -93,8 +80,10 @@ watch(
             <p v-if="card.heal">{{ "💖" + card.heal }}</p>
           </div>
         </div>
-        <div v-if="card.description && card.id !== 0" class="absolute" :class="size === 'normal' ? `top-5 right-5` : `top-5 right-5`">
-          <img :src="`/img/showSpecial/${card.company}.png`" class="absolute" />
+        <div v-if="card.description && card.id !== 0" class="relative"
+          :class="size === 'normal' ? `-top-24 -right-16` : `-top-32 -right-20 w-12`">
+          <img :src="`/img/showSpecial/${card.company}.png`" class="absolute"
+            :class="size === 'normal' ? `w-6` : `w-8`" />
         </div>
       </VDuringPress>
     </div>
