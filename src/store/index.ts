26f8bffd53bacab1631d7010c860c7c7a0656fa1
nasubi-dev <r916nis@gmail.com>
2025-07-13 21:12:10 +0900

    status調整

--- src/store/index.ts
+++ src/store/index.ts
@@ -84,8 +84,8 @@ const usePlayerStore = defineStore("playerData", () => {
     isSaleZeroHungry: false,
     field: [],
     status: {
-      hp: 30,
-      hungry: 30,
+      hp: 300,
+      hungry: 0,
       maxHp: 300,
       maxHungry: 100,
     },
@@ -214,8 +214,8 @@ const usePlayerStore = defineStore("playerData", () => {
       isSaleZeroHungry: false,
       field: [],
       status: {
-        hp: 30,
-        hungry: 30,
+        hp: 300,
+        hungry: 0,
         maxHp: 300,
         maxHungry: 100,
       },
@@ -293,8 +293,8 @@ const useEnemyPlayerStore = defineStore("enemyPlayerData", () => {
     isSaleZeroHungry: false,
     field: [],
     status: {
-      hp: 30,
-      hungry: 30,
+      hp: 300,
+      hungry: 0,
       maxHp: 300,
       maxHungry: 100,
     },
@@ -353,8 +353,8 @@ const useEnemyPlayerStore = defineStore("enemyPlayerData", () => {
       isShopSale: false,
       isSaleZeroHungry: false,
       status: {
-        hp: 30,
-        hungry: 30,
+        hp: 300,
+        hungry: 0,
         maxHp: 300,
         maxHungry: 100,
       },
